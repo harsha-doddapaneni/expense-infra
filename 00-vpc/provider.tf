4ece2b85ee2ec6e6fc5067b640b146bbd3ebde0e
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-29 16:11:59 +0530

    expense-infra

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -5,13 +5,13 @@ terraform {
       version = "5.94.1"
     }
   }
-
   backend "s3" {
-    bucket = "81s-remot-state"
-    key    = "expense-vpc"
-    region = "us-east-1"
-    dynamodb_table = "81s-locking-dev"
+    bucket       = "81s-remot-state"
+    key          = "expense-vpc"
+    region       = "us-east-1"
+    use_lockfile = true
   }
+
 }
 
 provider "aws" {
