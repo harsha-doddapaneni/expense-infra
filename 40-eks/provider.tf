ffa0321191e7caf129f29dce02766fbbb234ee92
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-27 13:13:46 +0530

    expense

--- 40-eks/provider.tf
+++ 40-eks/provider.tf
@@ -1,21 +1,46 @@
-terraform {
-  required_version = ">= 1.3.0"
+# terraform {
+#   required_version = ">= 1.3.0"
 
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.54.1" # Pin to latest stable 5.x version (avoids 6.x conflicts)
+#     }
+#   }
+
+#   backend "s3" {
+#     bucket         = "81s-remot-statt-dev"
+#     key            = "expense-eks"
+#     region         = "us-east-1"
+#     dynamodb_table = "81s-locking-dev"
+#   }
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.54.1" # Pin to latest stable 5.x version (avoids 6.x conflicts)
+      version = "5.54.1"
     }
-  }
-
-  backend "s3" {
-    bucket         = "81s-remot-statt-dev"
-    key            = "expense-eks"
-    region         = "us-east-1"
-    dynamodb_table = "81s-locking-dev"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+  alias  = "default"
+}
+
+# Enforce for all modules
+provider "aws" {
+  region = "us-east-1"
+  alias  = "eks"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "kms"
 }
