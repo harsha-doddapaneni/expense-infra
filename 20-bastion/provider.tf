5a644191da324f83170dfc622f5a4df9c861a1cd
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-27 11:56:19 +0530

    expense

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -1,20 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.94.1"
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0"
     }
   }
 
   backend "s3" {
-    bucket = "81s-remot-statt-dev"
-    key    = "expense-bastion"
-    region = "us-east-1"
+    bucket         = "81s-remot-statt-dev"
+    key            = "expense-bastion"
+    region         = "us-east-1"
     dynamodb_table = "81s-locking-dev"
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
