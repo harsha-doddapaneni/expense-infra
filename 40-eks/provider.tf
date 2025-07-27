3a0fcc2d3ea530a00ad53dbbabf96b03e7b2f983
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-27 13:01:51 +0530

    expense

--- 40-eks/provider.tf
+++ 40-eks/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"  # Stick to the latest 5.x release
+      version = "~> 5.0" # Use latest 5.x
     }
   }
 
